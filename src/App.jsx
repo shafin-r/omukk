a1bc68b67a54733f7f709379194a69ec76546182
fever-machine-enigma <shafin808s@gmail.com>
2024-07-18 14:12:29 +0600

    improved contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,57 @@
 import { CgMouse } from "react-icons/cg";
 import { RiMenu4Fill } from "react-icons/ri";
 import { RiCloseFill } from "react-icons/ri";
-import { useState } from "react";
+import { HiArrowLongRight } from "react-icons/hi2";
+import { useEffect, useState } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [circlePos, setCirclePos] = useState({ top: "50%", left: "50%" });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const { innerWidth, innerHeight } = window;
+
+      const centerX = innerWidth / 2;
+      const centerY = innerHeight / 2;
+      const distanceX = clientX - centerX;
+      const distanceY = clientY - centerY;
+
+      const scale = 0.05; // Adjust this value to control the sensitivity
+      const newLeft = centerX + distanceX * scale;
+      const newTop = centerY + distanceY * scale;
+
+      setCirclePos({
+        left: `${newLeft}px`,
+        top: `${newTop}px`,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  });
 
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className="px-6 bg-[#0B0B0B] h-full">
-      <img
-        src="/logo-icon-white.svg"
-        alt="logo - white"
-        className="px-4 fixed inset-0 opacity-10 m-auto"
-      />
+    <div className="px-6 lg:px-32 bg-[#0B0B0B] h-full">
+      <div
+        className="lg:block hidden px-4 fixed  opacity-10  m-auto rounded-full w-[600px] h-[600px] bg-gradient-to-t from-black to-[#B8BBFF]"
+        style={{
+          top: circlePos.top,
+          left: circlePos.left,
+          transform: "translate(-50%, -50%)",
+        }}
+      ></div>
       <button
         onClick={toggleNav}
-        className="fixed top-4 right-4 z-50 p-2 rounded"
+        className="fixed top-6 right-6 z-50 p-2 rounded"
       >
         <RiMenu4Fill size={30} className="text-white" />
       </button>
@@ -65,25 +97,29 @@ function App() {
         } transition-transform duration-300 ease-in-out`}
       >
         <section className="h-screen flex flex-col justify-center items-center gap-10">
-          <img src="/logo-text-white.svg" alt="" />
+          <img src="/logo-text-white.svg" alt="" className="lg:w-3/4" />
           <CgMouse size={40} className="text-white" />
         </section>
         <section
-          className="h-screen flex flex-col items-center gap-32"
+          className="h-screen flex flex-col lg:flex-row lg:justify-center gap-40 items-center "
           id="about"
         >
-          <h1 className="text-white text-7xl font-Inter font-semibold tracking-tighter">
-            About
-          </h1>
-          <p className="text-white text-xl font-Inter tracking-tight text-justify">
-            Omukk is a Bangladesh-based technology solution team dedicated to
-            help startups and businesses to grow their impacts. <br /> <br />
-            Besides contact-based works, Omukk has its own projects that are
-            constantly tested and upgraded for better uses.
-          </p>
+          <div className="lg:w-1/2 lg:flex lg:items-center lg:justify-end lg:h-screen">
+            <h1 className=" text-white text-7xl lg:text-[10rem] font-Inter font-semibold tracking-tighter">
+              About
+            </h1>
+          </div>
+          <div className="lg:w-1/2 lg:h-screen lg:flex lg:items-center">
+            <p className="lg:w-3/4 text-white text-xl lg:text-2xl font-Inter tracking-tighter">
+              Omukk is a Bangladesh-based technology solution team dedicated to
+              help startups and businesses to grow their impacts. Besides
+              contact-based works, Omukk has its own projects that are
+              constantly tested and upgraded for better uses.
+            </p>
+          </div>
         </section>
         <section
-          className="h-screen lg:h-auto flex flex-col items-center justify-evenly"
+          className="h-screen flex flex-col items-center justify-evenly"
           id="services"
         >
           <div className="flex flex-col gap-4 items-center">
@@ -105,7 +141,7 @@ function App() {
           </h1>
         </section>
         <section
-          className="flex flex-col gap-10 items-center h-auto"
+          className="flex flex-col gap-10 items-center h-screen"
           id="works"
         >
           <h1 className="text-white font-Inter tracking-tighter text-6xl font-semibold">
@@ -133,33 +169,66 @@ function App() {
           </div>
         </section>
         <section
-          className="h-screen lg:h-auto border-r-2 border-l-2"
+          className="h-screen lg:h-auto flex flex-col gap-16"
           id="contact"
         >
-          <div className="p-8 border-t-2"></div>
-          <div className="p-6 border-t-2 border-b-2 flex flex-col items-center gap-6">
-            <img src="/logo-icon-white.svg" alt="" className="" />
-            <h1 className="font-Inter text-3xl text-white font-semibold tracking-tighter">
-              Want to talk with us?
-            </h1>
+          <div className="p-8 flex items-center justify-center">
+            <img src="/logo-icon-white.svg" alt="" className="h-10 w-10" />
           </div>
-          <div className="flex justify-center items-center border-b-2 p-10">
-            <a
-              href="mailto:contact@omukk.dev"
-              className="font-Inter text-3xl text-white font-medium tracking-tighter"
-            >
-              contact@omukk.dev
-            </a>
-          </div>
-          <div className="flex justify-center items-center border-b-2">
-            <h1 className="font-Inter text-2xl text-white tracking-tighter p-6">
-              11:52 PM
+          <div className=" flex flex-col items-center gap-6 mt-10">
+            <h1 className="font-Inter text-5xl text-[#D9D9D9] font-semibold tracking-tighter text-center">
+              WANT TO TALK <br /> WITH US?
             </h1>
+            <button className="px-5 py-2 bg-white flex gap-2 items-center rounded-full">
+              <HiArrowLongRight size={30} />
+              <span className="font-Inter tracking-tighter text-xl">
+                Send us an email
+              </span>
+            </button>
           </div>
-          <div className="flex justify-center items-center border-b-2">
-            <h1 className="font-Inter text-2xl text-white tracking-tighter p-10 text-nowrap">
-              Dhaka, Bangladesh
-            </h1>
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-4">
+              <h1 className="font-Inter text-3xl text-white tracking-tighter font-medium">
+                SOCIAL
+              </h1>
+              <div className="flex gap-4">
+                <a
+                  href="https://www.instagram.com/omukk.official/"
+                  className="font-Inter font-light text-xl text-white px-3 py-2 ring-2 ring-white rounded-full tracking-tight"
+                >
+                  Instagram
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/omukk/"
+                  className="font-Inter font-light text-xl text-white px-3 py-2 ring-2 ring-white rounded-full tracking-tight"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h1 className="font-Inter text-3xl text-white tracking-tighter font-medium">
+                CONTACTS
+              </h1>
+              <div className="">
+                <a
+                  href="mailto:contact@omukk.dev"
+                  className="font-Inter text-xl text-white tracking-tight font-light"
+                >
+                  contact@omukk.dev
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h1 className="font-Inter text-3xl text-white tracking-tighter font-medium">
+                LOCATION
+              </h1>
+              <div className="">
+                <p className="font-Inter text-xl text-white tracking-tight font-light">
+                  Dhaka, Bangladesh
+                </p>
+              </div>
+            </div>
           </div>
         </section>
       </div>
